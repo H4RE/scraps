3a3cf0e6072896f14b92719666e12b0207849e83
H4RE <h4re.cpp@gmail.com>
2023-01-25 09:41:38 +0900

    add mergeSort

--- hs/app/Main.hs
+++ hs/app/Main.hs
@@ -1,7 +1,8 @@
 module Main (main) where
 
-import Sort(quickSort)
+import Sort(quickSort, mergeSort)
 
 main :: IO ()
 main = do
     print $ quickSort [10, 5, 8, 9, 0]
+    print $ mergeSort [10, 5, 8, 9, 0]
--- hs/src/Sort.hs
+++ hs/src/Sort.hs
@@ -1,5 +1,6 @@
 module Sort
   ( quickSort,
+    mergeSort,
   )
 where
 
@@ -9,3 +10,18 @@ quickSort (x : xs) = quickSort smaller ++ [x] ++ quickSort bigger
   where
     smaller = [v | v <- xs, v < x]
     bigger = [v | v <- xs, v > x]
+
+mergeSort:: (Ord a) => [a]->[a]
+mergeSort [] = []
+mergeSort [x] = [x]
+mergeSort xs = merge (mergeSort  first) (mergeSort second)
+  where
+    first = take n xs
+    second = drop n xs
+    n = length xs `div` 2
+    merge:: (Ord a) => [a] -> [a] ->[a]
+    merge xs [] = xs 
+    merge [] ys = ys 
+    merge (x:xs) (y:ys)
+      |x < y = x : merge xs (y:ys)
+      |otherwise = y: merge (x:xs) ys
